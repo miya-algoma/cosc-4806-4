e57a57216e8684f61d16c75d8aab70fed5436b17
miya-algoma <miya@algomau.ca>
2025-06-20 17:28:35 +0000

    Changed from May Iya, 219600000 to Reminders

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -4,7 +4,7 @@
         <div class="page-header" id="banner">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Hey, Mark Iya 219600000</h1>
+                    <h1>Reminders</h1>
                     <p class="lead"> <?= date("F jS, Y"); ?></p>
                 </div>
             </div>

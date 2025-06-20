48a61b0902d63b9cdcfad52fceb94204ba80ad80
miya-algoma <miya@algomau.ca>
2025-06-20 17:32:01 +0000

    Updated home view to display Reminders title with Aptos-style font

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,24 +1,26 @@
-    <?php require_once 'app/views/templates/header.php'; ?>
+<?php require_once 'app/views/templates/header.php'; ?>
 
-    <div class="container">
-        <div class="page-header" id="banner">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>Reminders</h1>
-                    <p class="lead"> <?= date("F jS, Y"); ?></p>
-                </div>
-            </div>
-        </div>
-        <style>
-          body {
-            font-family: 'Segoe UI', 'Helvetica Neue', sans-serif;
-          }
-        </style>
+<style>
+  body {
+    font-family: 'Segoe UI', 'Helvetica Neue', sans-serif;
+  }
+</style>
 
+<div class="container">
+    <div class="page-header" id="banner">
         <div class="row">
             <div class="col-lg-12">
-                <p> <a href="/logout">Click here to logout</a></p>
+                <h1>Reminders</h1>
+                <p class="lead"><?= date("F jS, Y"); ?></p>
             </div>
         </div>
+    </div>
 
-    <?php require_once 'app/views/templates/footer.php'; ?>
+    <div class="row">
+        <div class="col-lg-12">
+            <p><a href="/logout">Click here to logout</a></p>
+        </div>
+    </div>
+</div>
+
+<?php require_once 'app/views/templates/footer.php'; ?>

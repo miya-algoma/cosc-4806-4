e41c5615c4f5ab3d5d840e4f6196adb9fb56f1ee
miya-algoma <miya@algomau.ca>
2025-06-20 17:38:22 +0000

    Added create a new reminder

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -18,6 +18,7 @@
 
     <div class="row">
         <div class="col-lg-12">
+            <p><a href="/reminders/create">Create a new reminder</a></p>
             <p><a href="/logout">Click here to logout</a></p>
         </div>
     </div>
